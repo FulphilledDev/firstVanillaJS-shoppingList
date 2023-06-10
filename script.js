4143d2b13df6a587fb087cafb7fd134261689186
Philip Simpson <dev.phits@gmail.com>
2023-06-09 18:11:15 -0700

    Items Filter

--- script.js
+++ script.js
@@ -62,6 +62,22 @@ const clearItems = () => {
   checkUI();
 };
 
+const filterItems = (e) => {
+  const items = itemList.querySelectorAll('li');
+
+  const text = e.target.value.toLowerCase();
+
+  items.forEach((item) => {
+    const itemName = item.firstChild.textContent.toLowerCase();
+
+    if (itemName.indexOf(text) != -1) {
+      item.style.display = 'flex';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+};
+
 const checkUI = () => {
   const items = itemList.querySelectorAll('li');
 
@@ -78,5 +94,6 @@ const checkUI = () => {
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click', clearItems);
+itemFilter.addEventListener('input', filterItems);
 
 checkUI();
